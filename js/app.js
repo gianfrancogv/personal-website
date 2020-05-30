0d2bda240fe11ee5da4e2cbb7496351fdea99408
Gian Franco <gianfrancogv@gmail.com>
2020-05-29 21:34:30 -0300

    Cargando cards del portfolio

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
-$(document).ready(function(){
-    // add padding top to show content behind navbar
-    // $('body').css('padding-top', $('.navbar').outerHeight() + 'px')
+// $(document).ready(function(){
+//     // add padding top to show content behind navbar
+//     $('body').css('padding-top', $('.navbar').outerHeight() + 'px')
 
-    // detect scroll top or down
-    if ($('.smart-scroll').length > 0) { // check if element exists
-        var last_scroll_top = 0;
-        $(window).on('scroll', function() {
-            scroll_top = $(this).scrollTop();
-            if(scroll_top > last_scroll_top && last_scroll_top > 720) {
-                $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
-            }
-            else {
-                $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
-            }
-            last_scroll_top = scroll_top;
-        });
-    }
-});
+//     // detect scroll top or down
+//     if ($('.smart-scroll').length > 0) { // check if element exists
+//         var last_scroll_top = 0;
+//         $(window).on('scroll', function() {
+//             scroll_top = $(this).scrollTop();
+//             if(scroll_top > last_scroll_top && last_scroll_top > 720) {
+//                 $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+//             }
+//             else {
+//                 $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+//             }
+//             last_scroll_top = scroll_top;
+//         });
+//     }
+// });
