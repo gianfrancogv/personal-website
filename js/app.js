1925271e8182d3df109308b3d189c891557bf900
Gian Franco <gianfrancogv@gmail.com>
2020-06-02 15:56:51 -0300

    Finalizando funcionalidades de scroll y contact

--- js/app.js
+++ js/app.js
@@ -1,19 +1,38 @@
-// $(document).ready(function(){
-//     // add padding top to show content behind navbar
-//     $('body').css('padding-top', $('.navbar').outerHeight() + 'px')
+function scrollToTop (duration) {
 
-//     // detect scroll top or down
-//     if ($('.smart-scroll').length > 0) { // check if element exists
-//         var last_scroll_top = 0;
-//         $(window).on('scroll', function() {
-//             scroll_top = $(this).scrollTop();
-//             if(scroll_top > last_scroll_top && last_scroll_top > 720) {
-//                 $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
-//             }
-//             else {
-//                 $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
-//             }
-//             last_scroll_top = scroll_top;
-//         });
-//     }
-// });
+    const totalScrollDistance = document.scrollingElement.scrollTop;
+    let scrollY = totalScrollDistance, oldTimestamp = null;
+
+    function step (newTimestamp) {
+        if (oldTimestamp !== null) {
+            scrollY -= totalScrollDistance * (newTimestamp - oldTimestamp) / duration;
+            if (scrollY <= 0) return document.scrollingElement.scrollTop = 0;
+            document.scrollingElement.scrollTop = scrollY;
+        }
+        oldTimestamp = newTimestamp;
+        window.requestAnimationFrame(step);
+    }
+    window.requestAnimationFrame(step);
+}
+
+document.addEventListener('scroll', function (e) {
+    var top  = window.pageYOffset + window.innerHeight,
+        isVisible = top > document.querySelector('.about').offsetTop,
+        isVisible = top > document.querySelector('.skills-tools').offsetTop;
+
+     if (isVisible) {
+       document.querySelector('.about').classList.add('arriveLeft');
+     }
+     if (isVisible) {
+       document.querySelector('.skills-tools').classList.add('arriveRight');
+     }
+});
+
+$(document).ready(function(){
+    $('body').scrollspy({target: "body", offset: 80}); 
+
+    $('[data-spy="scroll"]').each(function () {
+        var $spy = $(this).scrollspy('refresh')
+    });
+
+});
